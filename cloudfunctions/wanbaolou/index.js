ed5fea6105e3155ee157c41a273e5c94c3f2d7c5
qqjt <itkid@live.com>
2020-02-06 22:46:13 +0800

    完善万宝楼金价数据计算

--- cloudfunctions/wanbaolou/index.js
+++ cloudfunctions/wanbaolou/index.js
@@ -1,4 +1,14 @@
-// 云函数入口文件
+/* 抓取万宝楼（jx3.seasunwbl.com）数据，计算各游戏服务器金价
+info: "6,150金"
+consignment_id: "542695001766117376"
+single_unit_size: 6150      每笔金数
+remain_unit_count: 2        笔数
+single_unit_price: 1000     每笔人民币售价（几毛钱）
+single_count_price: 615     金币/元
+seller_role_name: "慕***"    卖家
+remaining_time: 258925      剩余挂单时间（秒）
+ */
+
 const cloud = require('wx-server-sdk');
 cloud.init();
 
@@ -9,12 +19,118 @@ const request = require('request');
 const baseUrl = 'https://api-wanbaolou.xoyo.com/api/buyer/goods/list';
 const servers = [
   {
+    zone_id: "z01",
+    name: "电信一区",
+    servers: [
+      {
+        server_id: "gate0126",
+        name: "蝶恋花"
+      },
+      {
+        server_id: "gate0115",
+        name: "龙争虎斗"
+      },
+      {
+        server_id: "gate0101",
+        name: "长安城"
+      }
+    ]
+  },
+  {
+    zone_id: "z05",
+    name: "电信五区",
+    servers: [
+      {
+        server_id: "gate0519",
+        name: "幽月轮"
+      },
+      {
+        server_id: "gate0515",
+        name: "斗转星移"
+      },
+      {
+        server_id: "gate0524",
+        name: "剑胆琴心"
+      },
+      {
+        server_id: "gate0514",
+        name: "乾坤一掷"
+      },
+      {
+        server_id: "gate0505",
+        name: "唯我独尊"
+      },
+      {
+        server_id: "gate0502",
+        name: "梦江南"
+      }
+    ]
+  },
+  {
+    zone_id: "z08",
+    name: "电信八区",
+    servers: [
+      {
+        server_id: "gate0807",
+        name: "绝代天骄"
+      }
+    ]
+  },
+  {
+    zone_id: "z21",
+    name: "双线一区",
+    servers: [
+      {
+        server_id: "gate2107",
+        name: "天鹅坪"
+      },
+      {
+        server_id: "gate2106",
+        name: "破阵子"
+      }
+    ]
+  },
+  {
+    zone_id: "z22",
+    name: "双线二区",
+    servers: [
+      {
+        server_id: "gate2204",
+        name: "飞龙在天"
+      }
+    ]
+  },
+  {
+    zone_id: "z24",
+    name: "双线四区",
+    servers: [
+      {
+        server_id: "gate2402",
+        name: "青梅煮酒"
+      },
+      {
+        server_id: "gate2407",
+        name: "凌雪藏锋"
+      }
+    ]
+  },
+  {
     zone_id: "z29",
     name: "双线PVP搞事区",
     servers: [
       {
         server_id: "gate2901",
         name: "止戈为武"
+      }
+    ]
+  },
+  {
+    zone_id: "z31",
+    name: "电信PVP搞事区",
+    servers: [
+      {
+        server_id: "gate3101",
+        name: "百无禁忌"
       }
     ]
   }
@@ -43,7 +159,7 @@ function encodeQueryParams(data) {
  * @param server_id
  * @param page
  * @param count
- * @returns {Promise<unknown>}
+ * @returns {Promise<Array>}
  */
 function getList(zone_id, server_id, page, count) {
   const params = {
@@ -63,7 +179,7 @@ function getList(zone_id, server_id, page, count) {
     request({url: url, jar: true},
       (err, res) => {
         if (err) {
-          reject('net error');
+          reject('network error');
         }
         const body = res.body.slice(params.callback.length + 1, -2);
         const jsonRes = JSON.parse(body);
@@ -83,17 +199,16 @@ function getList(zone_id, server_id, page, count) {
  */
 async function wblSpider(zone_id, server_id) {
   let page = 1;
-  let count = 10;
+  const count = 10;
   let res = [];
-  let times = 1;
 
-  let list = await getList('z05', 'gate0515', page, count);
+  let list = await getList(zone_id, server_id, page, count);
   list.forEach(function (item) {
     res.push(item);
   });
   while (list.length === 10) {
     page += 1;
-    list = await getList('z05', 'gate0515', page, count);
+    list = await getList(zone_id, server_id, page, count);
     list.forEach(function (item) {
       res.push(item);
     });
@@ -102,10 +217,81 @@ async function wblSpider(zone_id, server_id) {
 }
 
 exports.main = async (event, context) => {
+  const ts = new Date().getTime();
+  let allServersSummary = {
+    serverName: '全服',
+    serverId: 'all',
+    minPrice: null,
+    avgPrice: null,
+    maxPrice: null,
+    totalCoins: 0,        // 总待售金币数
+    totalRMB: 0,          // 待售总金额（人民币）
+    totalUnits: 0,        // 总笔数
+    totalConsignments: 0, // 总挂单数,
+    timestamp: ts
+  };
   for (let i = 0; i < servers.length; ++i) {
     for (let j = 0; j < servers[i].servers.length; ++j) {
-      let consignments = await wblSpider(servers[i].zone_id, servers[i].servers[j].server_id);
-      console.log(consignments);
+      // 爬取所有服务器数据
+      const consignments = await wblSpider(servers[i].zone_id, (servers[i].servers)[j].server_id);
+      let serverSummary = {
+        serverName: (servers[i].servers)[j].name,
+        serverId: (servers[i].servers)[j].server_id,
+        minPrice: null,
+        avgPrice: null,
+        maxPrice: null,
+        totalCoins: 0,
+        totalRMB: 0,
+        totalUnits: 0,
+        totalConsignments: 0,
+        timestamp: ts
+      };
+      for (let k = 0; k < consignments.length; ++k) {
+        let item = consignments[k];
+        serverSummary.totalUnits += item.remain_unit_count;
+        allServersSummary.totalUnits += item.remain_unit_count;
+
+        serverSummary.totalConsignments += 1;
+        allServersSummary.totalConsignments += 1;
+
+        serverSummary.totalCoins += item.remain_unit_count * item.single_unit_size;
+        allServersSummary.totalCoins += item.remain_unit_count * item.single_unit_size;
+
+        serverSummary.totalRMB += item.remain_unit_count * item.single_unit_price;
+        allServersSummary.totalRMB += item.remain_unit_count * item.single_unit_price;
+
+        if (serverSummary.minPrice === null || serverSummary.minPrice > item.single_count_price) {
+          serverSummary.minPrice = item.single_count_price;
+        }
+        if (allServersSummary.minPrice === null || allServersSummary.minPrice > item.single_count_price) {
+          allServersSummary.minPrice = item.single_count_price;
+        }
+
+        if (serverSummary.maxPrice === null || serverSummary.maxPrice < item.single_count_price) {
+          serverSummary.maxPrice = item.single_count_price;
+        }
+        if (allServersSummary.maxPrice === null || allServersSummary.maxPrice < item.single_count_price) {
+          allServersSummary.maxPrice = item.single_count_price;
+        }
+      }
+      if (serverSummary.totalRMB) {
+        serverSummary.avgPrice = serverSummary.totalCoins * 100 / serverSummary.totalRMB;
+      }
+      // 数据入库
+      db.collection('wbl_coin_prices').add({
+        data: serverSummary
+      }).then((res) => {
+        console.log(res);
+      })
     }
   }
+  if (allServersSummary.totalRMB) {
+    allServersSummary.avgPrice = allServersSummary.totalCoins * 100 / allServersSummary.totalRMB;
+  }
+  db.collection('wbl_coin_prices').add({
+    data: allServersSummary
+  }).then((res) => {
+    console.log(res);
+  });
+  return allServersSummary;
 };
