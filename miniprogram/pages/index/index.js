bd236fab7491a7aab4b0a8b55de97913d7deeadb
qqjt <itkid@live.com>
2020-03-06 16:52:47 +0800

    成就百科-成就列表基本完成

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -4,14 +4,20 @@ Page({
       {
         name: 'accelerate',
         text: '加速宝典',
-        icon: '/images/accelerate.png',
+        icon: '/images/tools/accelerate.png',
         url: '../tools/accelerate/accelerate'
       },
       {
         name: 'coin',
         text: '金价助手',
-        icon: '/images/coin.png',
+        icon: '/images/tools/coin.png',
         url: '../tools/coin/coin'
+      },
+      {
+        name: 'achievement',
+        text: '成就百科',
+        icon: '/images/tools/achievement.png',
+        url: '../tools/achievement/achievement'
       }
     ],
   },
